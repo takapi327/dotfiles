9e1f45eb08716d4caa94733746dea88ad568b9f5
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-08-30 12:43:44 +0900

    Add tmux

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 chsh -s $(which zsh)
 source ~/dotfiles/.zshrc
