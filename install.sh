79810f64cedfa0e383daa09def00545d4326f408
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-09-27 15:10:22 +0900

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,55 @@
 #!/bin/zsh
 
+## sh エミュレーションモード
+emulate -R sh
+
+## シンボリックリンク
+ln -nfs ~/dotfiles/.zshrc ~/.zshrc
+source ~/.zshrc
+
+## install brew
+if ! which brew > /dev/null; then
+  echo "[INFO] install brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+  echo "[INFO] brew is already installed"
+fi
+
+## make directory .config
+mkdir -p ~/.config
+
+## install tmux
+if ! which tmux > /dev/null; then
+    echo "[INFO] install tmux"
+    brew install tmux
+    ln -nfs ~/dotfiles/.tmux.conf ~/.tmux.conf
+else
+    echo "[INFO] tmux is already installed"
+fi
+
+## install alacritty
+if ! which alacritty > /dev/null; then
+    echo "[INFO] install alacritty"
+    brew cask install alacritty
+    ln -nfs ~/dotfiles/alacritty ~/.config/alacritty
+else
+    echo "[INFO] alacritty is already installed"
+fi
+
+## install neovim
+if ! which neovim > /dev/null; then
+    echo "[INFO] install neovim"
+    brew install neovim
+    ln -nfs ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+    ln -nfs ~/dotfiles/nvim/dein.vim ~/.config/nvim/dein.vim
+    ln -nfs ~/dotfiles/nvim/lazy.vim ~/.config/nvim/lazy.vim
+    ln -nfs ~/dotfiles/colors/hybrid.vim ~/.config/colors/hybrid.vim
+else
+    echo "[INFO] neovim is already installed"
+fi
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-chsh -s $(which zsh)
-source ~/dotfiles/.zshrc
+#ln -sf ~/dotfiles/.zshrc ~/.zshrc
+#ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+#chsh -s $(which zsh)
+#source ~/dotfiles/.zshrc
